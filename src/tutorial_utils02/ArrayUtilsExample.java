71b4f7c581f69d4fed88bc53e0d392c90d8e889a
=mehdithe <=mehdi.cheracher@gmail.com>
2017-06-25 11:32:26 +0000

    util classes tutorial

---
+++ src/tutorial_utils02/ArrayUtilsExample.java
@@ -0,0 +1,96 @@
+package tutorial_utils02;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Arrays class utility methods examples
+ *
+ * @Author Mehdi Maick
+ */
+public class ArrayUtilsExample {
+
+    public static void main(String[] args) {
+
+        Integer[] arr = {1, 2, 5, 6, 7};
+        /*********************************************/
+                    /* Sorting */
+        /********************************************/
+
+        // the usual way
+        Arrays.sort(arr);
+        // you can also do it like this
+        Arrays.sort(arr, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                return Integer.compare(a, b);
+            }
+        });
+        // to sort in reverse order
+        Arrays.sort(arr, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                return Integer.compare(b, a);
+            }
+        });
+        // or if using java 8, to sort in reverse order
+        Arrays.sort(arr, Comparator.comparing(a -> -a));
+
+        /*********************************************/
+                    /* Binary Search */
+        /********************************************/
+
+        // the usual way
+        Arrays.binarySearch(arr, 2);
+        // search within a range
+        Arrays.binarySearch(arr, 0, 3, 2);
+        // or you specify you own comparator for costume classes
+        Arrays.binarySearch(arr, 2, Integer::compare);
+
+        /*********************************************/
+                    /* Copy */
+        /********************************************/
+
+        // copy of given range
+        Integer[] range = Arrays.copyOfRange(arr, 0, 4);
+        // creates a copy of the array starting from the beginning
+        // if the given length is bigger than the size of the original array
+        // null are used to fill the res
+        Integer[] integers = Arrays.copyOf(arr, 3);
+
+        /*********************************************/
+                    /* debugging */
+        /********************************************/
+
+        // gives a nice string representation of the array
+        String arrayAsString = Arrays.toString(arr);
+        // this one works even if the array has many dimensions
+        String multiDimAsString = Arrays.deepToString(arr);
+
+        /*********************************************/
+                    /* other */
+        /********************************************/
+
+        // fills the given array with the given value
+        Arrays.fill(arr, 34);
+        // returns true if both the arrays are equal false otherwise
+        Arrays.equals(arr, integers);
+
+        // constructs a list object based on the given parameter array,
+        List<Integer> list = Arrays.asList(1, 3, 4, 5);
+        Arrays.asList(arr);
+
+        // converts an array into a java8 stream , if you fill like doing some
+        // functional programming, although don't recommend it in competitive programming
+        // it's slow
+        Stream<String> binary = Arrays.stream(arr).filter(e -> e % 2 == 0).map(Integer::toBinaryString);
+        binary.forEach(System.out::println);
+
+        // this constructs a prefix array, based on the passed function (in this example a prefix sum array)
+        // it's faster for really large arrays
+        Arrays.parallelPrefix(arr, (a, b) -> a + b);
+    }
+
+}
---
+++ src/tutorial_utils02/CollectionUtilsExample.java
@@ -0,0 +1,81 @@
+package tutorial_utils02;
+
+import java.util.*;
+
+/**
+ * @Author Mehdi Maick
+ */
+public class CollectionUtilsExample {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        /*********************************************/
+                    /* Sorting */
+        /********************************************/
+
+        // the usual way
+        Collections.sort(list);
+        // you can also do it like this
+        Collections.sort(list, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                return Integer.compare(a, b);
+            }
+        });
+        // to sort in reverse order
+        Collections.sort(list, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                return Integer.compare(b, a);
+            }
+        });
+        // or if using java 8, to sort in reverse order
+        Collections.sort(list, Comparator.comparing(a -> -a));
+
+
+        /*********************************************/
+                    /* Binary Search */
+        /********************************************/
+
+        // the usual way
+        Collections.binarySearch(list, 2);
+        // or you specify you own comparator for costume classes
+        Collections.binarySearch(list, 2, Integer::compare);
+
+        /*********************************************/
+                    /* Copy */
+        /********************************************/
+
+        // copies the old list into the new list
+        // each element in the src list will keep the same index in the new List
+        // if the new List is larger than the old list, the other elements are unaffected
+
+        List<Integer> newList = new ArrayList<>();
+        Collections.copy(newList, list);
+
+        /*********************************************/
+                    /* other */
+        /********************************************/
+
+        // fills the given list with the given object
+        Collections.fill(list, 2);
+        // returns the frequency of the given object in the list
+        // based on it's equal method
+        int frequency = Collections.frequency(list, 2);
+        // randomly shuffle the elements of the list
+        Collections.shuffle(list);
+        // reverse the order of the elements in the list
+        Collections.reverse(list);
+        // rotate the elements in the list by the given distance
+        // the element in index i will be the element previously at index (i - dist + size) % size after the rotation
+        Collections.rotate(list, 2);
+        // returns the maximum/minimum element of the list, based on the natural order
+        // you can specify a comparator as a second parameter to add costume logic
+        Integer max = Collections.max(list);
+        Integer min = Collections.min(list);
+
+        // it swaps the elements in the given indices
+        Collections.swap(list, 0, 3);
+    }
+}
